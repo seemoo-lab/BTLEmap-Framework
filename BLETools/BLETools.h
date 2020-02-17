b57aff1ab375d87dc02bb6e18e5eab331a45dd6d
Alexander Heinrich <aheinrich@seemoo.tu-darmstadt.de>
2020-02-17 15:18:37 +0100

    Initial Commit

---
+++ BLETools/BLETools.h
@@ -0,0 +1,19 @@
+//
+//  BLETools.h
+//  BLETools
+//
+//  Created by Alex - SEEMOO on 17.02.20.
+//  Copyright © 2020 SEEMOO - TU Darmstadt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BLETools.
+FOUNDATION_EXPORT double BLEToolsVersionNumber;
+
+//! Project version string for BLETools.
+FOUNDATION_EXPORT const unsigned char BLEToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BLETools/PublicHeader.h>
+
+

6663c4c2da26f73a9264bfcf789eb800a4f58b2b
Alexander Heinrich <aheinrich@seemoo.tu-darmstadt.de>
2020-03-17 10:34:05 +0100

    Adding scan option for privileged deamons

--- Sources/BLETools/BLEReceiver.swift
+++ Sources/BLETools/BLEReceiver.swift
@@ -49,7 +49,10 @@ class BLEReceiver: NSObject {
             var scanOptions = [String: Any]()
             #if os(iOS)
             scanOptions["CBCentralManagerScanOptionIsPrivilegedDaemonKey"] = NSNumber(booleanLiteral: true)
+            scanOptions["kCBScanOptionIsPrivilegedDaemon"] = NSNumber(booleanLiteral: true)
+            scanOptions["kCBMsgArgIsPrivilegedDaemon"] = NSNumber(booleanLiteral: true)
             #endif
+//            scanOptions[CBCentralManagerScanOptionAllowDuplicatesKey] = NSNumber(booleanLiteral: false)
             self.centralManager.scanForPeripherals(withServices: nil, options: scanOptions)
             self.isScanning = true
         }else {
